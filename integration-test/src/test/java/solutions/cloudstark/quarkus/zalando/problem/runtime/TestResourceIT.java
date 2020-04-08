0f06f3890ffdc1cd45c07fc7b5ea18b2124ff9e0
Lars Martin <CrystalMethod@users.noreply.github.com>
2020-04-08 22:48:49 +0200

    add native tests (#48)

--- integration-test/src/test/java/solutions/cloudstark/quarkus/zalando/problem/runtime/TestResourceIT.java
+++ integration-test/src/test/java/solutions/cloudstark/quarkus/zalando/problem/runtime/TestResourceIT.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2020 SMB GmbH
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package solutions.cloudstark.quarkus.zalando.problem.runtime;
 
 import static io.restassured.RestAssured.given;
@@ -16,10 +32,12 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.startsWith;
 
+import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
+@DisabledOnNativeImage
 public class TestResourceIT {
 
   @Test
